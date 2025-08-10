00b20da734abce2e674fc2cb64fbd88891935383
Estefania0711 <laeslobu@gmail.com>
2025-08-10 11:28:15 -0500

    Primer comentario

---
+++ proyecto/src/test/java/com/clase/proyecto/AplicacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clase.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
