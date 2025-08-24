2ad011c647245ec30250829bb3f5b2451dccd2d0
Laura <laeslobu@gmail.com>
2025-08-23 21:51:51 -0500

    add profesor and alumno crud

--- proyecto/src/main/java/com/clase/proyecto/controller/ProfesorController.java
+++ proyecto/src/main/java/com/clase/proyecto/controller/ProfesorController.java
@@ -8,20 +8,35 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/profesores")
 public class ProfesorController {
-
-    private final ProfesorService profesorService;
-
-    public ProfesorController(ProfesorService profesorService) {
-        this.profesorService = profesorService;
-    }
-    @GetMapping
-    public Page<Profesor> getAll(@RequestParam int page, @RequestParam int size) {
-        PageRequest pageable = PageRequest.of(page, size);
+    private  ProfesorService profesorService;
+    public Page<Profesor> getAll(@RequestParam Integer page, @RequestParam Integer size) {
+        Pageable pageable = PageRequest.of(page, size);
         return profesorService.findAll(pageable);
+    }
+    @GetMapping("/{id}")
+    public Profesor getById(@RequestParam Integer id) {
+        return profesorService.findById(id)
+                .orElseThrow(() -> new RuntimeException("Profesor not found with id: " + id));
+
+    }
+    @PostMapping
+    public Profesor create(@RequestBody Profesor profesor) {
+        return profesorService.create(profesor);
+    }
+
+    @PutMapping("/{id}")
+    public Profesor update(@PathVariable Integer id, @RequestBody Profesor profesor) {
+        return profesorService.update(id, profesor);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        profesorService.deleteById(id);
     }
 }
